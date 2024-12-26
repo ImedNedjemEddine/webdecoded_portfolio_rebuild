76cadb7f6eafd03189355a91e17be40c6d0f3dcf
nedjem <justmrpepe@gmail.com>
2024-12-26 01:05:29 +0100

    last changes

--- src/app/components/contact.jsx
+++ src/app/components/contact.jsx
@@ -13,20 +13,25 @@ export function Contact() {
       </h2>
 
 
-      <div className="justify-center items-center w-[715px]">
-        <div className="flex max-ms:flex-col pr-4 pl-4">
-            <input className="m-1 h-14 w-[350px] bg-[#2C3333] rounded-xl" type="text" />
-            <input className="m-1 h-14 w-[350px] bg-[#2C3333] rounded-xl" type="text" />
+      <div className="flex flex-col justify-center items-center ">
+        <div className="flex max-sm:flex-col w-full ">
+            <input placeholder="Full Name" className="m-1 pl-3 h-14 w-[350px] max-sm:w-full bg-[#3C4045] rounded-xl" type="text" />
+            <input placeholder="Email Address" className="m-1 pl-3 h-14 w-[350px] max-sm:w-full bg-[#3C4045] rounded-xl" type="text" />
         </div>
-        <div className="flex max-ms:flex-col pr-4 pl-4">
-            <input className="m-1 h-14 w-[350px] bg-[#2C3333] rounded-xl" type="text" />
-            <input className="m-1 h-14 w-[350px] bg-[#2C3333] rounded-xl" type="text" />
+        <div className="flex max-sm:flex-col ">
+            <input placeholder="Mobile phone" className="m-1 pl-3 h-14 w-[350px] max-sm:w-full bg-[#3C4045] rounded-xl" type="text" />
+            <input placeholder="Email Subject" className="m-1 pl-3 h-14 w-[350px] max-sm:w-full bg-[#3C4045] rounded-xl" type="text" />
+        </div> 
+        <div className="w-full">
+        <textarea placeholder="your message ..." className=" p-4 pl-3 max-sm:h-[200px] max-sm:w-full h-[400px] w-[700px] bg-[#3C4045] rounded-xl"/>
         </div>
-        <div className="p-4">
-        <textarea className="h-[400px] w-full bg-[#2C3333] rounded-xl"/>
+        <div className="justify-center items-center">
+        <button
+              className="shadow-xy1 mt-6 hover:shadow-[rgba(116,246,255,0.8)] hover:shadow-xy1 shadow-[rgba(116,246,255,0.8)] px-12 py-3 rounded-full bg-gradient-to-br from-[#01E8F9] to-[#4D5EF1] w-full sm:w-fit hover:bg-slate-200 text-[#0F1012] mr-4"
+            >
+              Submit 
+            </button>
         </div>
-        
-        <button ><p>submit</p></button>
       </div>
     </div>
   );
